ce8c13c228c60f67bbc4ddcf91d182888b61c19f
imdhairyathakkar-spec <imdhairyathakkar@gmail.com>
2025-10-01 16:36:45 +0530

    Create exp-a.cpp

---
+++ exp-a.cpp
@@ -0,0 +1,25 @@
+//Dhairya Thakkar
+//prn 24070123037
+//B3
+#include <iostream>
+using namespace std ;
+int main(){
+    cout << "input the number " ; //input
+    int n;
+    cin >> n ; //input assigning to a variable
+    if (n<0){  //if condition
+        cout << "number is negative" ; //output if condition true
+    }
+    else if (n>0)
+    {
+        cout << "number is positive" ;
+    }
+    else{
+    cout << "number is zero" ;}
+    
+return 0;
+}
+
+/* output:
+input the number 34
+number is positive */
